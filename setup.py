8ad0f0fb52fc84e6a644df7c8f733e306dfe0323
Shromona MacBook <shromona.ghosh@berkeley.edu>
2017-09-28 12:57:22 -0700

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(name='active_testing',
+      author='Shromona Ghosh',
+      author_email='shromona.ghosh@berkeley.edu',
       install_requires=[
           'numpy',
           'GPy',
